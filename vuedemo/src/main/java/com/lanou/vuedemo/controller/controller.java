ffe8c3670d3e8a76908bae20e61894aa31c979c0
zyp1887415157 <1554361032@qq.com>
2018-12-12 14:42:59 +0800

    最后一次

---
+++ vuedemo/src/main/java/com/lanou/vuedemo/controller/controller.java
@@ -0,0 +1,14 @@
+package com.lanou.vuedemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+
+    @RequestMapping("/test")
+    public String test() {
+        return "hello";
+    }
+}
